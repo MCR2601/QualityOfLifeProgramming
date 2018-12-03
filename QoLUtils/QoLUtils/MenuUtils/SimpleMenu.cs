6dba362265b871c770b6d989305e99c17895007f
MCR <der.marcreiterer@gmail.com>
2018-12-03 14:21:01 +0100

    imported QoLUtils
    They include the following:
    - SimpleMenu
       Allows for easy to use menu with fluent api builder
    - testing project for simple menu

---
+++ QoLUtils/QoLUtils/MenuUtils/SimpleMenu.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QoLUtils.MenuUtils
+{
+    public class SimpleMenu
+    {
+        string Name;
+
+        bool quickInput = false;
+
+        List<MenuTopic> Topics = new List<MenuTopic>();
+
+        private SimpleMenu(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void CallMenu()
+        {   
+            string input;
+            int output;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("*** " + Name + " ***");
+                for (int i = 0; i < Topics.Count; i++)
+                {
+                    Console.WriteLine(i + ")\t" + Topics[i].TopicName);                    
+                }
+                if (Topics.Count<=10 && quickInput)
+                {
+                    input = Console.ReadKey().KeyChar.ToString();
+                    System.Threading.Thread.Sleep(100);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    input = Console.ReadLine();
+                }             
+            } while (!int.TryParse(input, out output));
+            MenuTopic topic = Topics[output];
+            topic.Call();
+        }
+
+
+        public class SimpleMenuBuilder
+        {
+            SimpleMenu menu;
+
+            public SimpleMenuBuilder(string MenuName)
+            {
+                menu = new SimpleMenu(MenuName);
+            }
+
+            public SimpleMenuBuilder AddTopic(string name, MenuCallable c, InputType i, object d = null)
+            {
+                menu.Topics.Add(new MenuTopic(name, c, i, d));
+                return this;
+            }
+
+            public SimpleMenuBuilder EnableQuickInput(bool tick = true)
+            {
+                menu.quickInput = tick;
+                return this;
+            }
+            
+
+            public SimpleMenu Build()
+            {
+                return menu;
+            }
+        }
+        public enum InputType
+        {
+            None,
+            Int,
+            Line
+        }
+        private class MenuTopic
+        {
+            public string TopicName;
+            public MenuCallable calling;
+            public InputType input;
+            public object data;
+
+            public MenuTopic(string name, MenuCallable c, InputType i, object d = null)
+            {
+                TopicName = name;
+                calling = c;
+                input = i;
+                data = d;
+            }
+
+            public void Call()
+            {
+                object line = "";
+                switch (input)
+                {
+                    case InputType.None:
+                        break;
+                    case InputType.Int:
+                        Console.WriteLine("Enter an Integer: ");
+                        try
+                        {
+                            line = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Wrong Input");
+                            System.Threading.Thread.Sleep(500);
+                            return;
+                        }
+                        break;
+                    case InputType.Line:
+                        Console.WriteLine("String Input:");
+                        line = Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+                calling.Invoke(line, data);
+            }
+
+            
+        }   
+    }
+}
---
+++ QoLUtils/TestingApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QoLUtils.MenuUtils;
+
+
+namespace TestingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMenu menu;
+            menu = new SimpleMenu.SimpleMenuBuilder("Test Menu")
+                .AddTopic("Something", DoSomething, SimpleMenu.InputType.None)
+                .AddTopic("Number", PrintANumber, SimpleMenu.InputType.Int)
+                .AddTopic("String", PrintString, SimpleMenu.InputType.Line)
+                .AddTopic("Multiply by 4", Multiply, SimpleMenu.InputType.Int, 4)
+                .AddTopic("Multiply by 5", Multiply, SimpleMenu.InputType.Int, 5)
+                .AddTopic("Multiply by 3",Multiply, SimpleMenu.InputType.Int, 3)
+                .AddTopic("Beeeep",LALALALA,SimpleMenu.InputType.None)
+                .EnableQuickInput()
+                .Build();
+
+            while (true)
+            {
+                menu.CallMenu();
+                Console.ReadKey();
+            }
+
+        }
+        public static void LALALALA(object nix, object data)
+        {
+            Console.Beep();
+            Console.WriteLine("BEEEEEEEEP");
+        }
+        public static void DoSomething(object nix, object data)
+        {
+            Console.WriteLine("did something");
+        }
+        public static void PrintANumber(object num, object data)
+        {
+            Console.WriteLine("Number "+num);
+        }
+        public static void PrintString(object line, object data)
+        {
+            Console.WriteLine("The line " + line);
+        }
+        public static void Multiply(object num, object other)
+        {
+            Console.WriteLine((int)num * (int)other);
+        }
+    }
+}
